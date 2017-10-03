f7861e9fd577531282d8ae72795418aa8c1b99e3
Stacey Walker <stacey@catalyst-eu.net>
2017-10-03 15:16:50 +0100

    WR277073: select and save institutional tags as well as own
    Change-Id: Id95c1481af1d49be429bab61c2f3d631ea47e43e

--- lib/user.php
+++ lib/user.php
@@ -1823,7 +1823,7 @@ function load_user_institutions($userid) {
         SELECT u.institution, ' . db_format_tsfield('ctime') . ',' . db_format_tsfield('u.expiry', 'membership_expiry') . ',
                u.studentid, u.staff, u.admin, i.displayname, i.theme, i.registerallowed, i.showonlineusers,
                i.allowinstitutionpublicviews, i.logo' . $logoxs . ', i.style, i.licensemandatory, i.licensedefault,
-               i.dropdownmenu, i.skins, i.suspended
+               i.dropdownmenu, i.skins, i.suspended, i.tags
         FROM {usr_institution} u INNER JOIN {institution} i ON u.institution = i.name
         WHERE u.usr = ? ORDER BY i.priority DESC', array($userid))) {
         return $institutions;
