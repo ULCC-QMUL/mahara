bc9e48d13e589e48043258b6bd7b150ffd90bb59
Maria Sorica <maria.sorica@catalyst-eu.net>
2017-10-19 11:30:14 +0100

    WR277070: Add an event that shares/unshares QM Dashboard when group member role gets changed.
    Change-Id: I37613f6bd05e0731a5fd9328cbda9fa139a279e3

--- htdocs/lib/upgrade.php
+++ htdocs/lib/upgrade.php
@@ -916,6 +916,7 @@ function core_install_firstcoredata_defaults() {
         'activateuser',
         'userjoinsgroup',
         'userleavesgroup',
+        'userchangegrouprole',
         'saveartefact',
         'deleteartefact',
         'deleteartefacts',
